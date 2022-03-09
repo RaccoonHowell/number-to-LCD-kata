e49129d792d3c7d4f35db28a468bb801fd1d8263
Rachel Howell <rac777blue@hotmail.com>
2022-03-09 18:19:50 +0000

    passed test "returns LCD style 8 when called with 8"

--- number_to_lcd.rb
+++ number_to_lcd.rb
@@ -7,7 +7,8 @@ class Converter
             4 => "   \n|_|\n  |",
             5 => " - \n|_ \n _|",
             6 => " - \n|_ \n|_|",
-            7 => " - \n  |\n  |"
+            7 => " - \n  |\n  |",
+            8 => " - \n|_|\n|_|"
         }
 
         lcd_numbers[number]
