c97b67976c0fbaa8d19841a822e9766082cf6155
Rachel Howell <rac777blue@hotmail.com>
2022-03-09 17:52:20 +0000

    passed test "returns LCD style 4 when called with 4"

--- number_to_lcd.rb
+++ number_to_lcd.rb
@@ -1,6 +1,8 @@
 class Converter
     def convert number
-        if number == 3
+        if number == 4
+            "   \n|_|\n  |"
+        elsif number == 3
             " - \n _|\n _|"
         elsif number == 2
             " - \n _|\n|_ "
